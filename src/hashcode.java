cf52d13c75025571347ec30dad56cf870e6c664f
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-24 15:02:48 +0800

    重写hashcode和equals方法用于比较

--- src/hashcode.java
+++ src/hashcode.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+
 /**
  * 重写hashcode和equals方法用于比较
  * */
@@ -17,7 +16,9 @@ class People{
     public void setAge(int age){
         this.age = age;
     }
-
+    /**
+     hashcode,equal两个方法依赖于易变数据，不可行
+     * */
     @Override
     public int hashCode() {
         // TODO Auto-generated method stub
@@ -36,7 +37,7 @@ public class hashcode {
     public static void main(String[] args) {
 
         People p1 = new People("Jack", 12);
-        //System.out.println(p1.hashCode());
+        System.out.println(p1.hashCode());
 
         HashMap<People, Integer> hashMap = new HashMap<People, Integer>();
         hashMap.put(p1, 1);
@@ -45,8 +46,12 @@ public class hashcode {
         /**
          *
          * “设计hashCode()时最重要的因素就是：无论何时，对同一个对象调用hashCode()都应该产生同样的值。
+         *
          * 如果在讲一个对象用put()添加进HashMap时产生一个hashCdoe值，而用get()取出时却产生了另一个hashCode值，那么就无法获取该对象了。
-         * 所以如果你的hashCode方法依赖于对象中易变的数据，用户就要当心了，因为此数据发生变化时，hashCode()方法就会生成一个不同的散列码”。
+         *
+         * 所以如果你的hashCode方法依赖于对象中易变的数据，用户就要当心了，因为此数据发生变化时，hashCode()方法就会生成一个不同的散列码”
+         *
+         * 设计hashCode方法和equals方法的时候，如果对象中的数据易变，则最好在equals方法和hashCode方法中不要依赖于该字段
          * */
         p1.setAge(1);
 
